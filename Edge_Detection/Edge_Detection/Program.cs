399a26da35a424f1aa33fd63ee716244746f99fd
Glence <glence.low@digipen.edu>
2025-02-05 17:09:20 +0800

    Add sobel logic and new package
    Add System.Drawing.Common package to use Bitmap
    
    Add a sobel class to contain the logic for the sobel filter
    
    Add a new function in program to convert images to gray scale to enable the sobel filter to work
    
    Add input for image to test sobel
    
    Add a photo folder to contain the images for testing

--- Edge_Detection/Edge_Detection/Program.cs
+++ Edge_Detection/Edge_Detection/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Edge_Detection
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace Edge_Detection
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+          string imagePath = Path.Combine(basePath, "Photo", "lena.png");
+          Bitmap image = new(imagePath);
+          Bitmap grayImage = ConvertToGrayScale(image);
+
+          Console.Write("applying sobel filter\n");
+          Bitmap sobelImage = Sobel.ApplySobel(grayImage);
+          string newImagePath = Path.Combine(basePath, "Photo", "sobelImage.jpg");
+          sobelImage.Save(newImagePath, ImageFormat.Jpeg);
+          Console.Write("sobel applied\n");
+        }
+
+        /// <summary>
+        /// a function to convert an image into grayscale
+        /// </summary>
+        static Bitmap ConvertToGrayScale(Bitmap image)
+        {
+          Bitmap grayImage = new(image.Width, image.Height);
+          for (int y = 0; y < image.Height; y++)
+          {
+            for (int x = 0; x < image.Width; x++)
+            {
+              Color pixel = image.GetPixel(x, y);
+              int gray = (int)(pixel.R * 0.3 +  pixel.G * 0.59 + pixel.B * 0.11);
+              grayImage.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+            }
+          }
+          return grayImage;
         }
     }
 }
---
+++ Edge_Detection/Edge_Detection/Sobel.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+
+namespace Edge_Detection
+{
+  /// <summary>
+  /// A edge detection using sobel 
+  /// </summary>
+  public static class Sobel
+  {
+    public static Bitmap ApplySobel(Bitmap image)
+    {
+      int width = image.Width;
+      int height = image.Height;
+      Bitmap sobelImage = new(width, height);
+
+      for (int y = 1; y < height - 1; y++)
+      {
+        for (int x = 1; x < width - 1; x++)
+        {
+          double gradX = 0, gradY = 0;
+
+          //apply the sobel matrix 
+          for (int i = -1; i < 2; i++)
+          {
+            for (int j = -1; j < 2; j++)
+            {
+              int pixVal = image.GetPixel(x + j, y + i).R;
+              gradX += pixVal * Matrix.sobelHorizontal[i + 1 , j + 1];
+              gradY += pixVal * Matrix.sobelVertical[i + 1, j + 1];
+            }
+          }
+
+          // get the magnitude of the gradient 
+          int mag = Math.Min(255, (int)Math.Sqrt(gradX * gradX + gradY * gradY));
+          sobelImage.SetPixel(x, y, Color.FromArgb(mag, mag, mag));
+        }
+      }
+
+      return sobelImage;
+    }
+  }
+}
