1f2a9c2b1aa24c067eede102788dec8a8da2011a
Glence <glence.low@digipen.edu>
2025-02-05 13:47:56 +0800

    Add base project and gitignore
    Add baseline project to work on
    Add gitignore for C#

---
+++ Edge_Detection/Edge_Detection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Edge_Detection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
