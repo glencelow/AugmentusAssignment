2d86d36258184d5c680167ff43a175a1e6a39e2d
Glence <glence.low@digipen.edu>
2025-02-05 15:56:41 +0800

    Add a new matrix class file
    Add multiple matrixes for sobel and prewitt to use

---
+++ Edge_Detection/Edge_Detection/Matrix.cs
@@ -0,0 +1,68 @@
+﻿namespace Edge_Detection
+{
+
+  /// <summary>
+  /// A matrix class which contains horizontal and vertical kernels for sobel and prewitt to apply 
+  /// </summary>
+  public static class Matrix
+  {
+
+    /// <summary>
+    /// horizontal matrix for sobel
+    /// </summary>
+    public static double[,] sobelHorizontal
+    {
+      get 
+      {
+        return new double[,]
+        { { -1, 0, 1 },
+          { -2, 0, 2 },
+          { -1, 0, 1 }, };
+      } 
+    }
+
+    /// <summary>
+    /// vertical matrix for sobel
+    /// </summary>
+    public static double[,] sobelVertical
+    {
+      get
+      {
+        return new double[,]
+        { {  1,  2,  1 },
+          {  0,  0,  0 },
+          { -1, -2, -1 }, };
+      }
+    }
+
+    /// <summary>
+    /// horizontal matrix for prewitt
+    /// </summary>
+    public static double[,] prewittHorizontal
+    {
+      get
+      {
+        return new double[,]
+        { { -1, 0, 1 },
+          { -1, 0, 1 },
+          { -1, 0, 1 }, };
+      }
+    }
+
+    /// <summary>
+    /// vertical matrix for prewitt
+    /// </summary>
+    public static double[,] prewittVertical
+    {
+      get
+      {
+        return new double[,]
+        { {  1,  1,  1 },
+          {  0,  0,  0 },
+          { -1, -1, -1 }, };
+      }
+    }
+
+
+  }
+}
