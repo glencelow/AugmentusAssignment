e577596a82079ed46ef1aeaf43d67687464660a5
Glence <glence.low@digipen.edu>
2025-02-06 16:38:25 +0800

    Add UnitTest  project for unit test
    Add UserinputUnitTest to test for user input
    
    Add testimages folder with images for ImageUnitTest
    Add ImageUnitTest to test for image that are filtered

---
+++ Edge_Detection/UnitTest/ImageUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using Edge_Detection;
+
+namespace UnitTest
+{
+  public class ImageUnitTest
+  {
+    /// <summary>
+    /// Test to see an exception thrown when input folder is empty
+    /// </summary>
+    [Fact]
+    public void ApplyFilterToAll_EmptyFolder_ThrowsException()
+    {
+      string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+      Directory.CreateDirectory(Path.Combine(basePath, "testemptyimage"));
+      string inputFolder = Path.Combine(basePath, "testemptyimage");
+      string outputFolder = Path.Combine(basePath, "output");
+
+      Assert.Throws<FileNotFoundException>(() => Program.ApplyFilterToAll(1, inputFolder, outputFolder));
+    }
+
+    /// <summary>
+    /// Test to see when the filter is applied
+    /// </summary>
+    [Fact]
+    public void ApplyFilterToAll_Valid_FilterApplied()
+    {
+      string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+      string inputFolder = Path.Combine(basePath, "testimages");
+      string outputFolder = Path.Combine(basePath, "output");
+
+      //clear output folder to ensure no overlap
+      System.IO.DirectoryInfo di = new DirectoryInfo(outputFolder);
+      foreach (FileInfo file in di.EnumerateFiles())
+      {
+        file.Delete();
+      }
+
+      Program.ApplyFilterToAll(1, inputFolder, outputFolder);
+
+      string[] inputJPGImages = Directory.GetFiles(inputFolder, "*.jpg");
+      string[] inputPNGImages = Directory.GetFiles(inputFolder, "*.png");
+
+      string[] outputJPGImages = Directory.GetFiles(outputFolder, "*.jpg");
+      string[] outputPNGImages = Directory.GetFiles(outputFolder, "*.png");
+
+      //the images saved would be the same as input, 1 for jpg and 1 for png
+      Assert.Equal(inputJPGImages.Length, outputJPGImages.Length);
+      Assert.Equal(inputPNGImages.Length, outputPNGImages.Length);
+    }
+  }
+}
---
+++ Edge_Detection/UnitTest/UserInputUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using Edge_Detection;
+
+namespace UnitTest
+{
+  public class UserInputUnitTest
+  {
+    /// <summary>
+    /// Test to see correct user input
+    /// </summary>
+    [Theory]
+    [InlineData("1", 1)] //  sobel
+    [InlineData("2", 2)] //  prewitt 
+    public void GetUserInput_Valid_ReturnExpectedValue(string input, int output)
+    {
+      int result = Program.GetUserInput(input);
+      Assert.Equal(output, result); 
+    }
+
+    /// <summary>
+    /// Test to see wrong user input
+    /// </summary>
+    [Theory]
+    [InlineData("5")]
+    [InlineData("-5")]
+    [InlineData("")]
+    [InlineData("qwe")]
+    public void GetUserInput_Invalid_ThrowException(string input)
+    {
+      Assert.Throws<ArgumentException>(() => Program.GetUserInput(input));
+    }
+  }
+}
