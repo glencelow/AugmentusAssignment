ee85a398bf66dc030efef67bbd88ed1645338d9c
Glence <glence.low@digipen.edu>
2025-02-06 23:19:10 +0800

    Drop matrix.cs and add respective matrix to each filter
    Drop matrix.cs as it wasnt need anymore
    
    Add the respective matrix to SobelFilter.cs and PrewittFilter.cs

--- Edge_Detection/Edge_Detection/PrewittFilter.cs
+++ Edge_Detection/Edge_Detection/PrewittFilter.cs
@@ -3,7 +3,34 @@ namespace Edge_Detection
 {
   public class PrewittFilter : EdgeDetectionFilter
   {
-    protected override double[,] getHorizontalMatrix() => Matrix.PrewittHorizontal;
-    protected override double[,] getVerticalMatrix() => Matrix.PrewittVertical;
+    /// <summary>
+    /// horizontal matrix for prewitt
+    /// </summary>
+    static double[,] PrewittHorizontal
+    {
+      get
+      {
+        return new double[,]
+        { { -1, 0, 1 },
+          { -1, 0, 1 },
+          { -1, 0, 1 }, };
+      }
+    }
+
+    /// <summary>
+    /// vertical matrix for prewitt
+    /// </summary>
+    static double[,] PrewittVertical
+    {
+      get
+      {
+        return new double[,]
+        { {  1,  1,  1 },
+          {  0,  0,  0 },
+          { -1, -1, -1 }, };
+      }
+    }
+    protected override double[,] getHorizontalMatrix() => PrewittHorizontal;
+    protected override double[,] getVerticalMatrix() => PrewittVertical;
   }
 }
--- Edge_Detection/Edge_Detection/SobelFilter.cs
+++ Edge_Detection/Edge_Detection/SobelFilter.cs
@@ -2,7 +2,35 @@
 {
   public class SobelFilter : EdgeDetectionFilter
   {
-    protected override double[,] getHorizontalMatrix() => Matrix.SobelHorizontal;
-    protected override double[,] getVerticalMatrix() => Matrix.SobelVertical;
+    /// <summary>
+    /// horizontal matrix for sobel
+    /// </summary>
+    static double[,] SobelHorizontal
+    {
+      get
+      {
+        return new double[,]
+        { { -1, 0, 1 },
+          { -2, 0, 2 },
+          { -1, 0, 1 }, };
+      }
+    }
+
+    /// <summary>
+    /// vertical matrix for sobel
+    /// </summary>
+    static double[,] SobelVertical
+    {
+      get
+      {
+        return new double[,]
+        { {  1,  2,  1 },
+          {  0,  0,  0 },
+          { -1, -2, -1 }, };
+      }
+    }
+
+    protected override double[,] getHorizontalMatrix() => SobelHorizontal;
+    protected override double[,] getVerticalMatrix() => SobelVertical;
   }
 }
